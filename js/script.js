974e56467eed77a0050daa8dc6396bf42c18050a
akshayakannan0503-arch <akshayakannan0503@gmail.com>
2026-03-06 17:34:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,59 +1,62 @@
+let serial = 1
+
 function addRow(){
 
-	let table = document.getElementById("invoiceTable")
+let table = document.getElementById("billTable")
 
-	let row = table.insertRow()
+let row = table.insertRow()
 
-	row.innerHTML = `
-	<td><input type="date"></td>
-	<td><input type="text"></td>
-	<td><input type="text"></td>
-	<td><input type="text"></td>
-	<td><input type="number" class="qty"></td>
-	<td><input type="number" class="weight"></td>
-	<td><input type="number" class="tonnage"></td>
-	<td><input type="number" class="rate"></td>
-	<td class="rowTotal">0</td>
-	`
-
+row.innerHTML = `
+<td>${serial++}</td>
+<td><input type="date"></td>
+<td><input type="text"></td>
+<td><input type="text"></td>
+<td><input type="text"></td>
+<td><input type="number"></td>
+<td><input type="number"></td>
+<td class="tonnage"><input type="number"></td>
+<td class="rate"><input type="number"></td>
+<td class="rowTotal">0</td>
+`
 }
 
-function calculate(){
+function calculateTotal(){
 
-	let tonnage = document.querySelectorAll(".tonnage")
-	let rate = document.querySelectorAll(".rate")
-	let totalCells = document.querySelectorAll(".rowTotal")
+let rows = document.querySelectorAll("#billTable tr")
 
-	let subtotal = 0
+let subtotal = 0
 
-	for(let i=0;i<tonnage.length;i++){
+for(let i=1;i<rows.length;i++){
 
-		let total = tonnage[i].value * rate[i].value
-		totalCells[i].innerHTML = total.toFixed(3)
+let tonnage = rows[i].cells[7].children[0].value
+let rate = rows[i].cells[8].children[0].value
 
-		subtotal += total
+let total = tonnage * rate
 
-	}
+rows[i].cells[9].innerHTML = total.toFixed(3)
 
-	let service = subtotal * 0.02
-	let sgst = (subtotal + service) * 0.09
-	let cgst = (subtotal + service) * 0.09
+subtotal += total
+}
 
-	let grand = subtotal + service + sgst + cgst
+let service = subtotal * 0.02
+let sgst = (subtotal + service) * 0.09
+let cgst = (subtotal + service) * 0.09
 
-	document.getElementById("subtotal").innerHTML =
-		"Subtotal: "+subtotal.toFixed(3)
+let grand = subtotal + service + sgst + cgst
 
-	document.getElementById("service").innerHTML =
-		"Service Charge 2%: "+service.toFixed(3)
+document.getElementById("subtotal").innerHTML =
+"Subtotal : "+subtotal.toFixed(3)
 
-	document.getElementById("sgst").innerHTML =
-		"SGST 9%: "+sgst.toFixed(3)
+document.getElementById("service").innerHTML =
+"Service Charge 2% : "+service.toFixed(3)
 
-	document.getElementById("cgst").innerHTML =
-		"CGST 9%: "+cgst.toFixed(3)
+document.getElementById("sgst").innerHTML =
+"SGST 9% : "+sgst.toFixed(3)
 
-	document.getElementById("grand").innerHTML =
-		"Total Value: ₹"+grand.toFixed(2)
+document.getElementById("cgst").innerHTML =
+"CGST 9% : "+cgst.toFixed(3)
+
+document.getElementById("grandTotal").innerHTML =
+"Total Value : ₹"+grand.toFixed(2)
 
 }
